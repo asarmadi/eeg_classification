1ff27f43002175283cc43d50a79fed33d67aa92f
asarmadi <a.sarmadi@yahoo.com>
2023-11-08 16:36:36 -0500

    New arch test

--- models/cnn_lstm.py
+++ models/cnn_lstm.py
@@ -5,15 +5,15 @@ class LSTMConv(nn.Module):
 
     def __init__(self, config):
         super(LSTMConv, self).__init__()
-        self.conv_layer1 = nn.Sequential(nn.Conv1d(config.hidden_dim, 8*config.n_channels, kernel_size=11),nn.BatchNorm1d(8*config.n_channels),nn.ReLU(inplace=True))
-        self.conv_layer2 = nn.Sequential(nn.Conv1d(8*config.n_channels, 8*config.n_channels, kernel_size=11),nn.BatchNorm1d(8*config.n_channels),nn.ReLU(inplace=True))
-        self.conv_layer3 = nn.Sequential(nn.Conv1d(8*config.n_channels, 8*config.n_channels, kernel_size=11),nn.BatchNorm1d(8*config.n_channels),nn.ReLU(inplace=True))
+        self.conv_layer1 = nn.Sequential(nn.Conv1d(config.hidden_dim, 8*config.n_channels, kernel_size=101),nn.BatchNorm1d(8*config.n_channels),nn.ReLU(inplace=True))
+        self.conv_layer2 = nn.Sequential(nn.Conv1d(8*config.n_channels, 8*config.n_channels, kernel_size=101),nn.BatchNorm1d(8*config.n_channels),nn.ReLU(inplace=True))
+        self.conv_layer3 = nn.Sequential(nn.Conv1d(8*config.n_channels, 8*config.n_channels, kernel_size=101),nn.BatchNorm1d(8*config.n_channels),nn.ReLU(inplace=True))
 
         self.rnn = nn.LSTM(config.n_channels, config.hidden_dim, config.layer_dim, batch_first=True, dropout=0.1)
 
-        self.avgpool = nn.MaxPool2d(2,stride=2)
+        self.avgpool = nn.AvgPool2d(2,stride=2)
 
-        self.fc1 = nn.Linear(config.n_channels*4*985, config.hidden_dim)
+        self.fc1 = nn.Linear(config.n_channels*4*850, config.hidden_dim)
         self.bn1 = nn.BatchNorm1d(config.hidden_dim)
         self.fc2 = nn.Linear(config.hidden_dim, config.n_classes)
         self.sigmoid = nn.Sigmoid()
