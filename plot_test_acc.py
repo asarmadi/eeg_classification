6b0d01db5377bcd7a54f4751c8bbcb5440ac420f
Alireza Sarmadi <asarmadi@kcl-robotics.com>
2023-11-09 00:59:40 -0500

    Plotting updated

--- plot_test_acc.py
+++ plot_test_acc.py
@@ -2,36 +2,30 @@ import os
 import csv
 import matplotlib
 import matplotlib.pyplot as plt
+from utils.config import Config
 
-matplotlib.rcParams['lines.linewidth'] = 2
-matplotlib.rcParams['xtick.labelsize']=26
-matplotlib.rcParams['ytick.labelsize']=26
-matplotlib.rcParams['axes.labelsize']=29
-matplotlib.rcParams['figure.facecolor']='white'
-
-font = {'family' : 'normal',
-        'weight' : 'bold',
-        'size'   : 26}
-
-matplotlib.rc('font', **font)
-plt.rc('axes', labelsize=26)
+config = Config()
 
 filePath = './out/'
-csv_files = os.listdir(filePath)
+#csv_files = os.listdir(filePath)
+
 test_acc  = {}
 valid_acc = {}
 train_acc = {}
 
-for csv_file in csv_files:
-    with open(filePath+csv_file, newline='') as f:
+for train_idx in config.all_subjects:
+    with open(filePath+"test_results_"+str(train_idx)+".0.csv", newline='') as f:
            data = list(csv.reader(f))
-           train_acc[data[2][2]] = data[1][1]
-           test_acc[data[2][2]] = data[2][1]
-           valid_acc[data[3][2]] = data[3][1]
+           print(data)
+           train_acc[data[2][2][:-2]] = float(data[1][1])
+           test_acc[data[2][2][:-2]]  = float(data[2][1])
+           valid_acc[data[3][2][:-2]] = float(data[3][1])
 
 plt.figure(0)
 plt.plot(list(train_acc.keys()), list(train_acc.values()), '-r.', label='Train')
-plt.plot(list(test_acc.keys()), list(test_acc.values()), '-b.', label='Test')
+plt.plot(list(test_acc.keys()),  list(test_acc.values()),  '-b.', label='Test')
 plt.plot(list(valid_acc.keys()), list(valid_acc.values()), '-g.', label='Valid')
 plt.legend()
+plt.tight_layout()
 plt.savefig('./Figs/Accs.png')
+
