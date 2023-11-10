ba43de7bff487293cc79e97c3503eb794a331f95
Alireza Sarmadi <asarmadi@kcl-robotics.com>
2023-11-10 12:38:05 -0500

    Plotting Updated

--- plot_test_acc.py
+++ plot_test_acc.py
@@ -3,6 +3,7 @@ import csv
 import matplotlib
 import matplotlib.pyplot as plt
 from utils.config import Config
+from numpy import array
 
 config = Config()
 
@@ -16,10 +17,19 @@ train_acc = {}
 for train_idx in config.all_subjects:
     with open(filePath+"test_results_"+str(train_idx)+".0.csv", newline='') as f:
            data = list(csv.reader(f))
-           print(data)
            train_acc[data[2][2][:-2]] = float(data[1][1])
            test_acc[data[2][2][:-2]]  = float(data[2][1])
            valid_acc[data[3][2][:-2]] = float(data[3][1])
+
+print(array([train_acc[k] for k in train_acc]).mean())
+print(array([train_acc[k] for k in train_acc]).std())
+
+print(array([test_acc[k] for k in test_acc]).mean())
+print(array([test_acc[k] for k in test_acc]).std())
+
+print(array([valid_acc[k] for k in valid_acc]).mean())
+print(array([valid_acc[k] for k in valid_acc]).std())
+
 
 plt.figure(0)
 plt.plot(list(train_acc.keys()), list(train_acc.values()), '-r.', label='Train')
