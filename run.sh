af284bb3b8a11088a7a65081eb584d6357585bea
asarmadi <a.sarmadi@yahoo.com>
2023-12-07 02:23:47 -0500

    Added results

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ if [ $MAJVOTE -eq 1 ]; then
   MAJ_VOTE_ARG="--maj_vote"
 fi
 
-for TARGET in 1 2 3 4 6 7 8 9 12 13 14 15 16 17 19 1; do
+for TARGET in 1 2 3 4 6 7 8 9 12 13 14 15 16 17 19; do
 TEST_TARGET=${TARGET}
 
 python mat2h5Conversion.py --target_test ${TEST_TARGET} ${APPLY_STFT_ARG}
