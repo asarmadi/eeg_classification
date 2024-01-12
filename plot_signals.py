5d556c06fa7bb03ca3fb83c8ec9f16fc0211be08
Alireza <a.sarmadi@yahoo.com>
2024-01-11 20:15:24 -0500

    updated

--- plot_signals.py
+++ plot_signals.py
@@ -2,11 +2,12 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 from utils.config import Config
+from utils.utils import scale, stockwell
 from scipy import signal
 
-raw_vs_preprocess = False
+raw_vs_preprocess = True
 stft = False
-find_min_max = True
+find_min_max = False
 
 Subject   = 2
 Condition = 0
@@ -62,14 +63,39 @@ if raw_vs_preprocess:
     for channel in range(config.n_channels):
          pre_data_c = preprocessed_data[Trial,:,channel]
          raw_data_c = raw_data[Trial,:,channel]
+         raw_data_c_s = scale(raw_data[Trial,:,:])
+         raw_data_c_s = raw_data_c_s[:,channel]
+
+         raw_data_c_stockwell   = stockwell(raw_data_c,config, True)
+         raw_data_c_s_stockwell = stockwell(raw_data_c_s,config, True)
+         pre_data_c_stockwell   = stockwell(pre_data_c,config, True)
+         print(raw_data_c_s_stockwell.shape)
+
+         extent = (0, config.n_timepoints, config.fmin, config.fmax)
      
          fig = plt.figure(1)
-         axs = fig.subplots(2, 1)
+         axs = fig.subplots(3, 1)
          axs[0].plot(pre_data_c, 'b')
          axs[0].set_ylabel("Pre-Processed")
          axs[1].plot(raw_data_c, 'b')
          axs[1].set_ylabel("Raw")
+         axs[2].plot(raw_data_c_s, 'b')
+         axs[2].set_ylabel("Scaled Raw")
+         fig.suptitle(f"Subject {Subject}, Trial {Trial}, Condition {Condition}, Channel {channel}")
          plt.savefig('./Figs/1d_raw_vs_preprocessed/'+str(Subject)+'_Cond_'+str(Condition)+'_Trial_'+str(Trial)+'_Channel_'+str(channel)+'.png')
+         #plt.show()
+         plt.close()
+
+         fig = plt.figure(2)
+         axs = fig.subplots(3, 1)
+         axs[0].imshow(pre_data_c_stockwell, origin='lower', extent=extent, aspect="auto")
+         axs[0].set_ylabel("Pre-Processed")
+         axs[1].imshow(raw_data_c_stockwell, origin='lower', extent=extent, aspect="auto")
+         axs[1].set_ylabel("Raw")
+         axs[2].imshow(raw_data_c_s_stockwell, origin='lower', extent=extent, aspect="auto")
+         axs[2].set_ylabel("Scaled Raw")
+         fig.suptitle(f"Subject {Subject}, Trial {Trial}, Condition {Condition}, Channel {channel}")
+         plt.savefig('./Figs/1d_raw_vs_preprocessed/stockwell'+str(Subject)+'_Cond_'+str(Condition)+'_Trial_'+str(Trial)+'_Channel_'+str(channel)+'.png')
          #plt.show()
          plt.close()
 
