d9693b444b222ef88a1d1c00c5cc4869dba97d8c
Alireza <a.sarmadi@yahoo.com>
2024-01-22 18:32:18 -0500

    updated

--- run.sh
+++ run.sh
@@ -13,11 +13,11 @@ if [ $MAJVOTE -eq 1 ]; then
   MAJ_VOTE_ARG="--maj_vote"
 fi
 
-for TARGET in 1 2 3 4 6 7 8 9 12 13 14 15 16 17 19; do
+#for TARGET in 1 2 3 4 6 7 8 9 12 13 14 15 16 17 19; do
 TEST_TARGET=${TARGET}
 TRANSFORM=stockwell
 #python -W ignore mat2h5ConversionFull.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
-python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
+#python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
 #python -W ignore mat2h5ConversionSeparateFiles.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
 
 
@@ -25,7 +25,7 @@ python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transf
 NEPOCHS=11
 MODEL_TYPE=cnn
 TRANSFORM=stockwell
-python -W ignore main.py --lr ${LR} --batch_size ${BATCH_SIZE} --wd ${WD} --model_type ${MODEL_TYPE} --n_epochs ${NEPOCHS} --kernel_size ${KERNEL_SIZE} --transform ${TRANSFORM}
+#python -W ignore main.py --lr ${LR} --batch_size ${BATCH_SIZE} --wd ${WD} --model_type ${MODEL_TYPE} --n_epochs ${NEPOCHS} --kernel_size ${KERNEL_SIZE} --transform ${TRANSFORM}
 
 #### Trainin MLP using AE
 #NEPOCHS=201
@@ -37,4 +37,4 @@ python -W ignore main.py --lr ${LR} --batch_size ${BATCH_SIZE} --wd ${WD} --mode
 #### Testing MLP
 python -W ignore test_performance.py --model_type ${MODEL_TYPE} --batch_size ${BATCH_SIZE} --kernel_size ${KERNEL_SIZE} --transform ${TRANSFORM} ${MAJ_VOTE_ARG}
 
-done
+#done
