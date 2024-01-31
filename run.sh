8d5ed94c0566a1eab4a173601a211c49d74b8122
Alireza <a.sarmadi@yahoo.com>
2024-01-30 19:22:18 -0500

    Number of epochs increased

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+
 LR=0.0001
 WD=0.01
 BATCH_SIZE=32
@@ -13,7 +14,8 @@ if [ $MAJVOTE -eq 1 ]; then
   MAJ_VOTE_ARG="--maj_vote"
 fi
 
-for TARGET in 1 3 4 6 7 8 9 10 13 14 15 16 17 18; do
+for TARGET in 1 2 3 4 6 7 8 9 10 12 13 14 15 16 17 18; do
+#for TARGET in 7; do
 TEST_TARGET=${TARGET}
 TRANSFORM=1d
 #python -W ignore mat2h5ConversionFull.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
@@ -25,7 +27,7 @@ python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transf
 #for TARGET in 1 2 3 4 6 7 8 9 12 13 14 15 16 17 19; do
 #TEST_TARGET=${TARGET}
 ##### Training AE ######
-NEPOCHS=21
+NEPOCHS=31
 MODEL_TYPE=atcnet
 TRANSFORM=nothing
 python -W ignore main.py --lr ${LR} --batch_size ${BATCH_SIZE} --wd ${WD} --model_type ${MODEL_TYPE} --n_epochs ${NEPOCHS} --kernel_size ${KERNEL_SIZE} --transform ${TRANSFORM} --subject ${TEST_TARGET}
