9441370a695d84b0cd3f8800d62e6c8846f1a337
asarmadi <a.sarmadi@yahoo.com>
2024-02-12 18:10:15 -0500

    model adapted for wpt

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -7,7 +7,7 @@ class Net(nn.Module):
     def __init__(self, config, kernel_size):
         super(Net, self).__init__()
         k1 = kernel_size
-        print(k1)
+        kernel_size = 5
         if config.apply_gauss:
            in_channels = 2*config.mapping_size
         elif config.transform == 'stockwell':
@@ -22,34 +22,38 @@ class Net(nn.Module):
            h_shape = config.n_channels
            w_shape = config.window_len
            in_channels = 1
+        elif config.transform == 'wpt':
+           h_shape = 20
+           w_shape = config.window_len
+           in_channels = len(config.channels_list)
         else:
            w_shape = config.n_channels
            h_shape = config.freq_cut
            in_channels = 1
 
-        self.conv_layer1 = nn.Sequential(nn.Conv2d(in_channels, 60, kernel_size=(k1,kernel_size)),
+        self.conv_layer1 = nn.Sequential(nn.Conv2d(in_channels, 60, kernel_size=(k1,kernel_size), stride=(1,kernel_size)),
                            nn.ReLU(inplace=True))
 
         print(f'H: {h_shape} W:{w_shape}')
 
         h_shape = ((h_shape-k1)+1)
-        w_shape = ((w_shape-kernel_size)+1)
+        w_shape = ((w_shape-kernel_size)//kernel_size+1)
 
-        self.conv_layer2 = nn.Sequential(nn.Conv2d(60, 80, kernel_size=(k1,kernel_size)),
+        self.conv_layer2 = nn.Sequential(nn.Conv2d(60, 80, kernel_size=(k1,kernel_size), stride=(1,kernel_size)),
 #                           nn.BatchNorm2d(80),
                            nn.ReLU(inplace=True))
 
         h_shape = ((h_shape-k1) + 1)
-        w_shape = ((w_shape-kernel_size) + 1)
+        w_shape = ((w_shape-kernel_size)//kernel_size + 1)
 
-        self.conv_layer3 = nn.Sequential(nn.Conv2d(80, 80, kernel_size=(k1,kernel_size)),
+        self.conv_layer3 = nn.Sequential(nn.Conv2d(80, 80, kernel_size=(k1,kernel_size), stride=(1,kernel_size)),
 #                           nn.BatchNorm2d(80),
                            nn.ReLU(inplace=True),
                            nn.Dropout(0.5),
                            nn.AvgPool2d(2))
 
         h_shape = ((h_shape-k1) + 1)//2
-        w_shape = ((w_shape-kernel_size) + 1)//2
+        w_shape = ((w_shape-kernel_size)//kernel_size + 1)//2
 
 
         self.conv_layer4 = nn.Sequential(nn.Conv2d(80, 80, kernel_size=(k1,kernel_size)),
