285fc462e230d6e39b18ceca497d4db7f2817b53
asarmadi <a.sarmadi@yahoo.com>
2024-02-19 23:50:56 -0500

    fine tunning added

--- mat2h5Conversion.py
+++ mat2h5Conversion.py
@@ -26,10 +26,13 @@ elif config.apply_valid_set == 'double':
    data_sets = ['train', 'test']
 elif config.apply_valid_set == 'single':
    data_sets = ['train']
+elif config.apply_valid_set == 'fineTune':
+   data_sets = ['train', 'tune', 'test']
 
 print(f'Test Subjects:  {config.test_subjects}')
 print(f'Train Subjects: {config.train_subjects}')
 
+percent = 2
 path = config.file_path + "SF_"+config.data_path+"_MLdata.mat"
 #path = config.file_path + "SF_"+config.data_path+"_data_nopre.mat"
 
@@ -40,13 +43,21 @@ def find_subjects_list(data_type):
         subjects_list = config.test_subjects
     elif data_type == 'valid':
         subjects_list = config.valid_subjects
+    elif data_type == 'tune':
+        subjects_list = config.test_subjects
     return subjects_list
 
 def find_num_samples(num_trials, sub_list, data_type):
     n_samples = 0
     for sub in sub_list:
         for cond in config.conditions:
-            n_samples += config.n_windows*num_trials[sub][cond]
+            n_trials = num_trials[sub][cond]
+            if config.apply_valid_set == 'fineTune':
+               if data_type == 'tune':
+                  n_trials = n_trials*percent//10
+               elif data_type == 'test':
+                  n_trials  = n_trials*(10-percent)//10
+            n_samples += config.n_windows*n_trials
     return n_samples
 
 def find_num_trials(data_type):
@@ -59,15 +70,14 @@ def find_num_trials(data_type):
             ref = f["data"][condition][subject]
             eeg = np.array(f[ref])
             trials_shape[condition]=eeg.shape[0]
-#            trials_shape[condition]=17
         data_dict[subject]=trials_shape
     return data_dict
 
 
-def generate_data(data_type):
+def generate_data(data_type, tune_trials):
     u = 0
     subjects_list = find_subjects_list(data_type)
-    trials_list   = find_num_trials(data_type)
+    trials_list = find_num_trials(data_type)
 
     n_samples = find_num_samples(trials_list,subjects_list, data_type)
     print(n_samples)
@@ -93,16 +103,25 @@ def generate_data(data_type):
     f_data.create_dataset("subject",   (n_samples,))
     f_data.create_dataset("trial",     (n_samples,))
     f_data.create_dataset("condition", (n_samples,))
+    if data_type == 'tune':
+       tune_trials = {}
 
     f = h5py.File(path,'r',libver='latest')
-
     for subject in subjects_list:
+        trials_dict = {}
         for condition in config.conditions:
             ref = f["data"][condition][subject]
             eeg = np.array(f[ref])
             print(f'{data_type} subject#: {subject}, condition: {condition}')
-            sub_trials  = trials_list[subject][condition]
-            for i_trial in range(sub_trials):
+            sub_trials  = np.arange(trials_list[subject][condition])
+            if config.apply_valid_set == 'fineTune':
+               if data_type == 'tune':
+                  sub_trials = np.array(np.random.choice(sub_trials,trials_list[subject][condition]*percent//10,replace=False))
+                  trials_dict[condition] = sub_trials
+               elif data_type == 'test':
+                  sub_trials = np.setdiff1d(sub_trials, tune_trials[subject][condition])
+                  sub_trials = np.array(np.random.choice(sub_trials,trials_list[subject][condition]*(10-percent)//10,replace=False))
+            for i_trial in sub_trials:
 #                print(i_trial,trial_idx, sub_trials)
                 eeg_scaled = eeg[i_trial,:,:]
                 eeg_scaled = preprocess_signal(config,eeg_scaled)
@@ -125,9 +144,11 @@ def generate_data(data_type):
                     f_data["condition"][u]    = condition
                     f_data["subject"][u]  = subject
                     u += 1
-
+        tune_trials[subject] = trials_dict
     f_data.close()
+    return tune_trials
 
+tune_trials_ = {}
 for dataSet in data_sets:
-    generate_data(dataSet)
+    tune_trials_ = generate_data(dataSet, tune_trials_)
 
