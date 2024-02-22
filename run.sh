6cad3ad74170389eb1428f2a27cec573a3485697
asarmadi <a.sarmadi@yahoo.com>
2024-02-22 02:30:22 -0500

    updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-LR=0.0005
+LR=0.00005
 WD=0.0
 BATCH_SIZE=16
 KERNEL_SIZE=5
@@ -13,12 +13,14 @@ if [ $MAJVOTE -eq 1 ]; then
   MAJ_VOTE_ARG="--maj_vote"
 fi
 
-#for TARGET in 1 2 3 4 6 7 8 9 10 12 13 14 15 16 17 18; do
-for TARGET in 1 4 7 14 16 17 18; do
+for PERCENT in 1 2 3 4 5 6;do
+T_PERCENT=${PERCENT}
+for TARGET in 1 2 3 4 6 7 8 9 10 12 13 14 15 16 17 18; do
+#for TARGET in 1 4; do
 TEST_TARGET=${TARGET}
 TRANSFORM=1d
-#python -W ignore mat2h5ConversionFull.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
-python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
+python -W ignore mat2h5ConversionFull.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM} --percent ${T_PERCENT}
+#python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
 #python -W ignore mat2h5ConversionSeparateFiles.py --target_test ${TEST_TARGET} --apply_transform ${TRANSFORM}
 #done
 #python csp_test.py --subject ${TEST_TARGET}
@@ -26,7 +28,7 @@ python -W ignore mat2h5Conversion.py --target_test ${TEST_TARGET} --apply_transf
 #for TARGET in 1 2 3 4 6 7 8 9 12 13 14 15 16 17 19; do
 #TEST_TARGET=${TARGET}
 ##### Training AE ######
-NEPOCHS=31
+NEPOCHS=21
 MODEL_TYPE=atcnet
 TRANSFORM=nothing
 python -W ignore main.py --lr ${LR} --batch_size ${BATCH_SIZE} --wd ${WD} --model_type ${MODEL_TYPE} --n_epochs ${NEPOCHS} --kernel_size ${KERNEL_SIZE} --transform ${TRANSFORM} --subject ${TEST_TARGET}
@@ -45,4 +47,5 @@ python -W ignore test_performance.py --model_type ${MODEL_TYPE} --batch_size ${B
 #python -W ignore voting.py --model_type ${MODEL_TYPE} --batch_size ${BATCH_SIZE} --kernel_size ${KERNEL_SIZE} --transform ${TRANSFORM} ${MAJ_VOTE_ARG} --target_test ${TEST_TARGET}
 done
 
-python plot_test_acc.py
+python plot_test_acc.py --percent ${T_PERCENT}
+done
